006263d419014ce67e8da99823e04926f004f546
Clete Blackwell II <clete2@gmail.com>
2017-11-15 12:06:59 -0600

    Begin to implement the arq parser

--- imports/parsers/arq-email-parser.js
+++ imports/parsers/arq-email-parser.js
@@ -1,5 +1,16 @@
+const computerNameRegex = /Computer:\n(.*?)\n/;
+const userNameRegex = /User:\\n(.*?)\\n/;
+const destinationRegex = /Destination:\\n(.*?)\\n/;
+const folderStartEndScannedUploadedRegex = /Folder\\nStart Date\\nEnd Date\\nScanned\\nUploaded\\n\\n(.*?)\\n(.*?)\\n(.*?)\\n(.*?)\\n(.*?)\\n/;
+
 export const canParse = text => text !== null && (!!text.match(/\barq\b/i));
 
-const parse = text => text;
+const parse = (text) => {
+  const result = {};
+
+  [, result.computer] = computerNameRegex.exec(text);
+
+  return result;
+};
 
 export default parse;
--- imports/parsers/arq-email-parser.test.js
+++ imports/parsers/arq-email-parser.test.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 // eslint-disable-next-line import/no-unresolved
 import { assert } from 'meteor/practicalmeteor:chai';
-import { canParse, parse } from './arq-email-parser';
+import parse, { canParse } from './arq-email-parser';
 
+const errorResultText = 'Computer:\nmy MacBook Pro\n\nUser:\nme\n\nDestination:\nme.example.com\n\n\n\n \n\nFolder\nStart Date\nEnd Date\nScanned\nUploaded\n\nme\nNovember 9, 2017 at 7:29:02 PM EST\nNovember 10, 2017at 9:22:28 PM EST\n86.203 GB\n1.631 GB\n\n\n\n \n\n\nLog:\n\nArq Agent version 5.9.4 started backup session for sftp:\/\/backup@me.example.com:9482\/mnt\/backup\/arq\/me\/UUID-GOES-HERE on November 9, 2017 at 7:28:53 PM EST\n\n\nBackup session for \/Users\/me started on November 9, 2017 at 7:29:02 PM EST\nFound in-progress backup record for \/Users\/me\nSFTP server is unavailable.\n\nAborted.\n\nScanned 86.203 GB (40372 files)\nUploaded 1.631 GB\nBackup session for \/Users\/me ended on November 10, 2017 at 9:22:28 PM EST\n\n\nBackup session for sftp:\/\/backup@me.example.com:9482\/mnt\/backup\/arq\/me\/UUID-GOES-HERE ended on November 10, 2017 at 9:22:29 PM EST (1 error)\n\n';
+const successResultText = 'Computer:\nmy MacBook Pro\n\nUser:\nme\n\nDestination:\nmy.example.com\n\n\n\n \n\nFolder\nStart Date\nEnd Date\nScanned\nUploaded\n\nme\nNovember 8, 2017 at 7:19:56 PM EST\nNovember 8, 2017 at 7:21:51 PM EST\n263.518 GB\n228.2 MB\n\n\n\n \n\n\nLog:\n\nArq Agent version 5.9.4 started backup session for sftp:\/\/backup@my.example.com:22\/rpool\/backup\/arq\/meMBP\/Some-GUID-HERE on November 8,2017 at 7:19:43 PM EST\n\n\nBackup session for \/Users\/me started on November 8, 2017 at 7:19:56 PM EST\nFound completed backup record for \/Users\/me\nSaved backup record for \/Users\/me\nScanned 263.518 GB (152147 files)\nUploaded 228.2 MB\nBackup session for \/Users\/me ended on November 8, 2017 at 7:21:51 PM EST\n\n\nBackup session for sftp:\/\/backup@my.example.com:22\/rpool\/backup\/arq\/meMBP\/Some-GUID-HERE ended on November 8, 2017 at 7:21:51 PM EST\n\n';
 
 if (Meteor.isServer) {
   describe('arq email parser canParse method', () => {
-    it('can parse a report with the word arq in it', function () {
+    it('can parse a report with the word arq in it', () => {
       assert.isTrue(canParse('Arq'));
     });
 
@@ -32,15 +34,17 @@ if (Meteor.isServer) {
     });
 
     it('can parse a real result with errors', () => {
-      const resultText = 'Computer:\nmy MacBook Pro\n\nUser:\nme\n\nDestination:\nme.example.com\n\n\n\n \n\nFolder\nStart Date\nEnd Date\nScanned\nUploaded\n\nme\nNovember 9, 2017 at 7:29:02 PM EST\nNovember 10, 2017at 9:22:28 PM EST\n86.203 GB\n1.631 GB\n\n\n\n \n\n\nLog:\n\nArq Agent version 5.9.4 started backup session for sftp:\/\/backup@me.example.com:9482\/mnt\/backup\/arq\/me\/UUID-GOES-HERE on November 9, 2017 at 7:28:53 PM EST\n\n\nBackup session for \/Users\/me started on November 9, 2017 at 7:29:02 PM EST\nFound in-progress backup record for \/Users\/me\nSFTP server is unavailable.\n\nAborted.\n\nScanned 86.203 GB (40372 files)\nUploaded 1.631 GB\nBackup session for \/Users\/me ended on November 10, 2017 at 9:22:28 PM EST\n\n\nBackup session for sftp:\/\/backup@me.example.com:9482\/mnt\/backup\/arq\/me\/UUID-GOES-HERE ended on November 10, 2017 at 9:22:29 PM EST (1 error)\n\n';
-
-      assert.isTrue(canParse(resultText));
+      assert.isTrue(canParse(errorResultText));
     });
 
     it('can parse a real result without errors', () => {
-      const resultText = 'Computer:\nmy MacBook Pro\n\nUser:\nme\n\nDestination:\nmy.example.com\n\n\n\n \n\nFolder\nStart Date\nEnd Date\nScanned\nUploaded\n\nme\nNovember 8, 2017 at 7:19:56 PM EST\nNovember 8, 2017 at 7:21:51 PM EST\n263.518 GB\n228.2 MB\n\n\n\n \n\n\nLog:\n\nArq Agent version 5.9.4 started backup session for sftp:\/\/backup@my.example.com:22\/rpool\/backup\/arq\/meMBP\/Some-GUID-HERE on November 8,2017 at 7:19:43 PM EST\n\n\nBackup session for \/Users\/me started on November 8, 2017 at 7:19:56 PM EST\nFound completed backup record for \/Users\/me\nSaved backup record for \/Users\/me\nScanned 263.518 GB (152147 files)\nUploaded 228.2 MB\nBackup session for \/Users\/me ended on November 8, 2017 at 7:21:51 PM EST\n\n\nBackup session for sftp:\/\/backup@my.example.com:22\/rpool\/backup\/arq\/meMBP\/Some-GUID-HERE ended on November 8, 2017 at 7:21:51 PM EST\n\n';
+      assert.isTrue(canParse(successResultText));
+    });
+  });
 
-      assert.isTrue(canParse(resultText));
+  describe('arq email parser parse method', () => {
+    it('Can determine the computer name in a valid result', () => {
+      assert.equal('my MacBook Pro', parse(successResultText).computer);
     });
   });
 }
